de95d1dc0ae074afdef51651b62b9ae1c8cbde87
xiaomlove <353856593@qq.com>
2021-03-31 16:40:15 +0800

    allagents.php show counts

--- public/allagents.php
+++ public/allagents.php
@@ -4,13 +4,13 @@ dbconn();
 loggedinorreturn();
 if (get_user_class() < UC_MODERATOR)
 	stderr("Error", "Permission denied.");
-$res2 = sql_query("SELECT agent,peer_id FROM peers  GROUP BY agent ") or sqlerr();
+$res2 = sql_query("SELECT agent,count(*) as counts FROM peers  GROUP BY agent order by agent asc") or sqlerr();
 stdhead("All Clients");
 print("<table align=center border=3 cellspacing=0 cellpadding=5>\n");
-print("<tr><td class=colhead>Client</td><td class=colhead>Peer ID</td></tr>\n");
+print("<tr><td class=colhead>Client</td><td class=colhead>Counts</td></tr>\n");
 while($arr2 = mysql_fetch_assoc($res2))
 {
-	print("</a></td><td align=left>{$arr2['agent']}</td><td align=left>{$arr2['peer_id']}</td></tr>\n");
+	print("</a></td><td align=left>{$arr2['agent']}</td><td align=left>{$arr2['counts']}</td></tr>\n");
 }
 print("</table>\n");
 stdfoot();
