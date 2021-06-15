533e1a61dc679a790ad42d5de0804e3f55bddd14
xiaomlove <i353856593@163.com>
2021-06-16 00:03:23 +0800

    special section deny text cht + en

---
+++ lang/en/lang_speical.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang_special = [
+    'std_sorry' => "Sorry...",
+    'std_permission_denied_only' => "Permission denied. Only ",
+    'std_or_above_can_view' => " or above could view Special section. <br /><br /> Please see the <a class=faqlink href=faq.php#id22><b>FAQ</b></a> for more information on different user classes and what they can do.<br /><br /><b></p>The " .$SITENAME." Staff</b>",
+];
