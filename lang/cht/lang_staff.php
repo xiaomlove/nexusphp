f8f580cb9235d097f63ad7b0611444b110bde2a7
xiaomlove <i353856593@163.com>
2021-06-17 22:47:15 +0800

    only vip or above can see staff

--- lang/cht/lang_staff.php
+++ lang/cht/lang_staff.php
@@ -27,7 +27,10 @@ $lang_staff = array
 	'text_apply_for_it' => "申請加入",
 	'text_forum_moderators' => "論壇版主",
 	'text_forum_moderators_note' => "論壇版主負責論壇的日常管理。",
-	'text_forums' => "版塊"
+	'text_forums' => "版塊",
+    'std_sorry' => "對不起...",
+    'std_permission_denied_only' => "你沒有該權限。只有",
+    'std_or_above_can_view' => "及以上等級的用戶才能查看管理組。<br /><br />請查看<a class=faqlink href=faq.php#id22><b>常見問題</b></a>了解更多關於用戶等級和權限的信息。<br /><br /><b></p>" .$SITENAME."管理組</b>",
 );
 
 ?>
