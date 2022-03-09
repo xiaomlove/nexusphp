cf7b962cc0567d0141f18b36d6cffce4d7c9c581
xiaomlove <i353856593@163.com>
2022-03-09 12:15:30 +0800

    install add constant NEXUS_START

--- nexus/Install/install_update_start.php
+++ nexus/Install/install_update_start.php
@@ -2,6 +2,7 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 0);
 define('IN_NEXUS', true);
+define('NEXUS_START', microtime(true));
 require ROOT_PATH . 'vendor/autoload.php';
 require ROOT_PATH . 'nexus/Database/helpers.php';
 require ROOT_PATH . 'include/constants.php';
