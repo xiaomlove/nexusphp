69c6a6f6d63baeec0da8fdb3826269feb88c2299
xiaomlove <i353856593@163.com>
2022-03-19 16:50:55 +0800

    release v1.6.2

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
-defined('VERSION_NUMBER') || define('VERSION_NUMBER', '1.6.1');
-defined('RELEASE_DATE') || define('RELEASE_DATE', '2022-03-18');
+defined('VERSION_NUMBER') || define('VERSION_NUMBER', '1.6.2');
+defined('RELEASE_DATE') || define('RELEASE_DATE', '2022-03-19');
 defined('IN_TRACKER') || define('IN_TRACKER', true);
 defined('PROJECTNAME') || define("PROJECTNAME","NexusPHP");
 defined('NEXUSPHPURL') || define("NEXUSPHPURL","https://nexusphp.org");
