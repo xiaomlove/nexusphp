2c41009111cfef74ddf2c116f8bf58daf7c43670
xiaomlove <i353856593@163.com>
2022-10-06 18:31:16 +0800

    fix trans

--- resources/lang/en/nexus.php
+++ resources/lang/en/nexus.php
@@ -4,4 +4,5 @@ return [
     'invalid_argument' => 'Invalid argument',
     'require_argument' => ':argument cannot be empty',
     'select_one_please' => 'Please select one',
+    'user_not_exists' => '(orphaned)',
 ];
--- resources/lang/zh_CN/nexus.php
+++ resources/lang/zh_CN/nexus.php
@@ -4,4 +4,5 @@ return [
     'invalid_argument' => '参数错误',
     'require_argument' => ':argument 不能为空',
     'select_one_please' => '请选择一项',
+    'user_not_exists' => '(无此帐户)',
 ];
