3d6ac852fa08d81ce63023d1b5d016ff254cf15e
Rey5 <mtylweb@gmail.com>
2023-05-07 04:39:15 +0800

    iyuu verification support

--- routes/third-party.php
+++ routes/third-party.php
@@ -2,4 +2,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('nastools/approve', [\App\Http\Controllers\AuthenticateController::class, 'nasToolsApprove']);
+Route::get('iyuu/verify', [\App\Http\Controllers\AuthenticateController::class, 'iyuuVerify']);
 
