dd977403ccde2b1cab96dded286b768bfd6f9483
xiaomlove <i353856593@163.com>
2023-05-14 15:50:11 +0800

    fix exam

--- include/constants.php
+++ include/constants.php
@@ -1,6 +1,6 @@
 <?php
-defined('VERSION_NUMBER') || define('VERSION_NUMBER', '1.7.37');
-defined('RELEASE_DATE') || define('RELEASE_DATE', '2023-05-08');
+defined('VERSION_NUMBER') || define('VERSION_NUMBER', '1.7.38');
+defined('RELEASE_DATE') || define('RELEASE_DATE', '2023-05-14');
 defined('IN_TRACKER') || define('IN_TRACKER', false);
 defined('PROJECTNAME') || define("PROJECTNAME","NexusPHP");
 defined('NEXUSPHPURL') || define("NEXUSPHPURL","https://nexusphp.org");
