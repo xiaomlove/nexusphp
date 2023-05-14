bf0f391a05f87c561ffc40b8a97ed3f2f2779c01
xiaomlove <i353856593@163.com>
2023-05-15 03:49:21 +0800

    version: 1.7.39

--- include/constants.php
+++ include/constants.php
@@ -1,13 +1,13 @@
 <?php
-defined('VERSION_NUMBER') || define('VERSION_NUMBER', '1.7.38');
-defined('RELEASE_DATE') || define('RELEASE_DATE', '2023-05-14');
+defined('VERSION_NUMBER') || define('VERSION_NUMBER', '1.7.39');
+defined('RELEASE_DATE') || define('RELEASE_DATE', '2023-05-15');
 defined('IN_TRACKER') || define('IN_TRACKER', false);
 defined('PROJECTNAME') || define("PROJECTNAME","NexusPHP");
 defined('NEXUSPHPURL') || define("NEXUSPHPURL","https://nexusphp.org");
 defined('NEXUSWIKIURL') || define("NEXUSWIKIURL","https://doc.nexusphp.org");
 defined('VERSION') || define("VERSION","Powered by <a href=\"aboutnexus.php\">".PROJECTNAME."</a>");
 defined('THISTRACKER') || define("THISTRACKER","General");
-defined('CONTENT_WIDTH') || define("CONTENT_WIDTH", 1200);
+defined('CONTENT_WIDTH') || define("CONTENT_WIDTH", 1200)
 defined('ROOT_PATH') || define('ROOT_PATH', dirname(__DIR__) . '/');
 defined('DEFAULT_TRACKER_URI') || define('DEFAULT_TRACKER_URI', '/announce.php');
 if (!defined('RUNNING_IN_OCTANE')) {
