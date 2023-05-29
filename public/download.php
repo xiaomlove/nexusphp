39acd85e42b031ae735b39bf342ab5e681c00f41
lgb <353856593@qq.com>
2023-05-29 20:16:15 +0800

    announce use passkey

--- public/download.php
+++ public/download.php
@@ -141,7 +141,7 @@ if (strlen($CURUSER['passkey']) != 32) {
 }
 $trackerReportAuthKey = $torrentRep->getTrackerReportAuthKey($id, $CURUSER['id'], true);
 $dict = \Rhilip\Bencode\Bencode::load($fn);
-$dict['announce'] = $ssl_torrent . $base_announce_url . "?authkey=$trackerReportAuthKey";
+$dict['announce'] = $ssl_torrent . $base_announce_url . "?passkey=" . $CURUSER['passkey'];
 do_log(sprintf("[ANNOUNCE_URL], user: %s, torrent: %s, url: %s", $CURUSER['id'] ?? '', $id, $dict['announce']));
 /**
  * does not support multi-tracker
