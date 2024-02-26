79fe56967a6b3b6912dcebce740747f90cf961c5
lgb <353856593@qq.com>
2024-02-26 23:44:17 +0800

    temp invite

--- app/Jobs/GenerateTemporaryInvite.php
+++ app/Jobs/GenerateTemporaryInvite.php
@@ -64,6 +64,8 @@ class GenerateTemporaryInvite implements ShouldQueue
             return;
         }
         $idArr = explode(",", $idStr);
+        $now = Carbon::now();
+        $expiredAt = Carbon::now()->addDays($this->days);
         foreach ($idArr as $uid) {
             try {
                 $hashArr = $toolRep->generateUniqueInviteHash([], $this->count, $this->count);
@@ -74,8 +76,8 @@ class GenerateTemporaryInvite implements ShouldQueue
                         'invitee' => '',
                         'hash' => $hash,
                         'valid' => 0,
-                        'expired_at' => Carbon::now()->addDays($this->days),
-                        'created_at' => Carbon::now(),
+                        'expired_at' => $expiredAt,
+                        'created_at' => $now,
                     ];
                 }
                 if (!empty($data)) {
