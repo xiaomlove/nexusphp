f3c1f2cdf8cddc37aba4ccd7123ac611cdc00d8b
xiaomlove <i353856593@163.com>
2024-03-12 01:52:26 +0800

    improve authorization page

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -11,8 +11,21 @@
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
     <style>
-        .passport-authorize .container {
-            margin-top: 30px;
+        .passport-authorize {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .passport-authorize .card {
+            /*padding: 40px;*/
+        }
+
+        .passport-authorize .card-header {
+            font-size: 24px;
+            text-align: center;
+            margin-bottom: 20px;
         }
 
         .passport-authorize .scopes {
@@ -25,7 +38,15 @@
         }
 
         .passport-authorize .btn {
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #00BFFF;
             width: 125px;
+            color: white;
+        }
+
+        .passport-authorize .btn-danger {
+            background-color: #a83838;
         }
 
         .passport-authorize .btn-approve {
@@ -38,7 +59,6 @@
     </style>
 </head>
 <body class="passport-authorize">
-<div class="flex-wrap">
     <div class="card card-default">
         <div class="card-header">
             Authorization Request
@@ -83,7 +103,6 @@
                 </form>
             </div>
         </div>
-    </div>
 </div>
 </body>
 </html>
