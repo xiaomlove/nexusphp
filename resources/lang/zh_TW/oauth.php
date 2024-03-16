5ac5e8342dd868df2f546790335def7f0f2604c0
xiaomlove <i353856593@163.com>
2024-03-16 23:16:48 +0800

    add oauth zh_TW translatioin

--- resources/lang/zh_TW/oauth.php
+++ resources/lang/zh_TW/oauth.php
@@ -6,4 +6,8 @@ return [
     'revoked' => '有效',
     'access_token' => '訪問令牌',
     'refresh_token' => '刷新令牌',
+    'authorization_request_title' => '授權請求',
+    'authorization_request_desc' => '正在請求獲取您賬號的訪問權限',
+    'btn_approve' => '授權',
+    'btn_deny' => '取消',
 ];
