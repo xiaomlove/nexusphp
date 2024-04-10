6ea03a2515a675d873abfd54e3041f006157af21
lgb <353856593@qq.com>
2024-04-10 12:31:34 +0800

    fix pcntl_* function check

--- nexus/Install/Install.php
+++ nexus/Install/Install.php
@@ -172,8 +172,8 @@ class Install
                 $output = [];
                 $command = "php -r 'var_export(function_exists(\"$fn\"));'";
                 $result = exec($command, $output, $result_code);
-                $lastThreeChars = substr(trim($output[0]), -3);
-                $exists = $lastThreeChars == 'true';
+                $lastFourChars = substr(trim($output[0]), -4);
+                $exists = $lastFourChars == 'true';
                 if (!$exists) {
                     $disabledFunctions[] = $fn;
                 }
