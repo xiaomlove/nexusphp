e30ae98bf5aaf6bc39b6537ad2906e094997e168
Koala Ng <me@koala.pm>
2025-06-02 01:15:50 +0800

    Update TechnicalInformation.php
    1. Added "Title" info into audio and subtitles items of mediainfo extra
    2. Corrected incorrect spelling of variables in getSubtitles function

--- nexus/Torrent/TechnicalInformation.php
+++ nexus/Torrent/TechnicalInformation.php
@@ -98,6 +98,9 @@ class TechnicalInformation
             if (!empty($values['Language'])) {
                 $audioInfoArr[] = $values['Language'];
             }
+            if (!empty($values['Title'])) {
+                $audioInfoArr[] = $values['Title'];
+            }
             if (!empty($values['Format'])) {
                 $audioInfoArr[] = $values['Format'];
             }
@@ -123,13 +126,16 @@ class TechnicalInformation
             }
             $audioInfoArr = [];
             if (!empty($values['Language'])) {
-                $audioInfoArr[] = $values['Language'];
+                $subtitlesInfoArr[] = $values['Language'];
+            }
+            if (!empty($values['Title'])) {
+                $subtitlesInfoArr[] = $values['Title'];
             }
             if (!empty($values['Format'])) {
-                $audioInfoArr[] = $values['Format'];
+                $subtitlesInfoArr[] = $values['Format'];
             }
-            if (!empty($audioInfoArr)) {
-                $result[$parentKey] = implode(" ", $audioInfoArr);
+            if (!empty($subtitlesInfoArr)) {
+                $result[$parentKey] = implode(" ", $subtitlesInfoArr);
             }
         }
         return $result;
